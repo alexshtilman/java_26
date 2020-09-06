7438c5e347ebce7a8ce4d08f859e0979105602e3
Alex Shtilman <10961645+alexshtilman@users.noreply.github.com>
2020-09-06 18:07:37 +0300

    Update Anagram.java

--- src/telran/words/Anagram.java
+++ src/telran/words/Anagram.java
@@ -9,19 +9,19 @@ public class Anagram {
 		if (word.length() == 0 || anagram.length() == 0 || word.length() != anagram.length() || word.equals(anagram))
 			return false;
 
-	/*simple first solution
-	int[] count = new int[256];
-        for (int i = 0; i < str1.length(); i++) {
-            count[str1.charAt(i)]++;
-            count[str2.charAt(i)]--;
-        }
-        for (int num : count) {
-            if (num != 0) {
-                return false;
-            }
-        }
-        return true;
-        */
+		/*simple first solution
+		int[] count = new int[256];
+	        for (int i = 0; i < str1.length(); i++) {
+	            count[str1.charAt(i)]++;
+	            count[str2.charAt(i)]--;
+	        }
+	        for (int num : count) {
+	            if (num != 0) {
+	                return false;
+	            }
+	        }
+	        return true;
+	        */
 		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
 		for (char c:word.toCharArray()) {
 			map.compute(c, (key, prev) -> prev != null ? prev + 1 : 1);
